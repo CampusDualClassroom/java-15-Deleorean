949d75b980d36a74cb763d3105e7c5449d5b6b99
Edugonzalez <eduu.gonzalez@gmail.com>
2024-11-20 20:14:42 +0100

    Ejercicio 15 hecho. Pa saber formatear la fecha y que pasara el test sudé bastante sangre.

--- src/main/java/com/campusdual/classroom/FreshMerchandise.java
+++ src/main/java/com/campusdual/classroom/FreshMerchandise.java
@@ -4,5 +4,38 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class FreshMerchandise extends Merchandise {
+    Date expirationDate = new Date(123, 4 , 4);
+    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
 
+    public FreshMerchandise(String name, String uniqueId, String responsibleId) {
+        super(name, uniqueId, responsibleId);
+    }
+
+    public FreshMerchandise(String name, String uniqueId, String responsibleId, int zone, String area, String shelf, int quantity, Date expirationDate) {
+        super(name, uniqueId, responsibleId, zone, area, shelf, quantity);
+    }
+
+    public Date getExpirationDate() {
+        return this.expirationDate;
+    }
+
+    @Override
+    public Object getSpecificData() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Localización: ");
+        builder.append(getLocation()); //No se usa el this. porque es un metodo de la clase padre
+        builder.append(". ");
+        builder.append("Fecha de caducidad: ");
+        builder.append(sdf.format(this.getExpirationDate()));//Este lleva el this. porque si que es de esta misma clase
+        return builder.toString();
+    }
+
+    public String getFormattedDate(Date expirationDate) {
+        return sdf.format(expirationDate);
+    }
+
+    public void printSpecificData() {
+        System.out.println(getSpecificData());
+    }
 }
+;
